b8b415bc960b4e4d95d612faebdccf7f979b05fd
Andy Meneely <andy.meneely@gmail.com>
2012-01-24 09:05:40 -0500

    adding a bunch of stuff

---
+++ prepgrades.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+$ZIP=$1
+
+# mkdir
+
+# unzip
+
+# Make this with digits
+# find . -name "*palindrome\(1\).c" -exec rm -rf {} \;
+
+# ls *palindrome.c | awk '{split($0,a," - "); system("mkdir \""a[1]"\"") }'
+
+#  ls *palindrome.c | awk '{split($0,a," - "); system("cp \""$0"\" \""a[1]"\"/palindrome.c") }'
---
+++ usage.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+#If number of command line parameters are not equal to 2
+#if [ "$#" -ne 2 ] 
+#then
+#	echo "Usage: $0 zipfile directory"
+#	exit 1
+#fi
+
+: ${2?"\nUsage: $0 firstparameter secondparameter"}
+
